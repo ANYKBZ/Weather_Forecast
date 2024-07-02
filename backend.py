9c0919037d552f17ef5ebdfbc3f72067ff67af57
ANYKBZ <alanz@V.hsd1.ca.comcast.net>
2024-07-01 22:37:26 -0700

    finish

--- backend.py
+++ backend.py
@@ -1,11 +1,14 @@
 import requests
-API_KEY = 75dcd59c7d0083abd85b77b793609c35
+API_KEY = "75dcd59c7d0083abd85b77b793609c35"
 
-def get_data(place, forecast_days, kind):
-    url = f"api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+def get_data(place, forecast_days):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
 
 if __name__ =="__main__":
     get_data()
--- main.py
+++ main.py
@@ -6,9 +6,20 @@ place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecasted days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} in {place}")
+if place:
+    try:
+        filtered_data = get_data(place, days)
+        if option == "Temperature":
+            temperature = [dict["main"]["temp"]/10 for dict in filtered_data]
+            print(type(temperature))
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-data = get_data(place,days,option)
-
-dates = [""]
-figure = px.line(x, y, labels=)
-st.plotly_chart()
+        if option == "Sky":
+            images = {"Clear": "weather_images/clear.png", "Clouds": "weather_images/cloud.png", "Rain": "weather_images/rain.png", "Snow": "weather_images/Snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_path = [images[condition] for condition in sky_conditions]
+            st.image(image_path, width=110)
+    except KeyError:
+        st.write("Incorrect City Name")
